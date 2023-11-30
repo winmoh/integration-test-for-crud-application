99f7da58cfa124470710145647a3a99dbee0a3c5
winmoh <mohamedaouinti2023@gmail.com>
2023-11-30 01:49:47 +0100

    adding mockMc as request simualator that help making http reuests without the need to run the server

--- src/test/java/com/integrationtest/first/integrationtest/IntegrationTestApplicationTests.java
+++ src/test/java/com/integrationtest/first/integrationtest/IntegrationTestApplicationTests.java
@@ -1,24 +1,38 @@
 package com.integrationtest.first.integrationtest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.integrationtest.first.integrationtest.Model.Product;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+
 @SpringBootTest(webEnvironment =SpringBootTest.WebEnvironment.RANDOM_PORT )
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 class IntegrationTestApplicationTests {
 
 	@LocalServerPort
 	private int port;
+	@Autowired
+	private MockMvc mvc;
 	private static String baseUrl="http://localhost:";
 	private static RestTemplate restTemplate;
 
@@ -36,16 +50,40 @@ class IntegrationTestApplicationTests {
 		baseUrl=baseUrl.concat(this.port+"").concat("/api/products");
 
 	}
+
+	//integration test for adding product using restTepmlate (need for starting a server)
 	@Test
 	public void addProductTest(){
 		Product prd=new Product("short",543,199);
 		Product product=restTemplate.postForObject(baseUrl.concat("/addProduct"),prd,Product.class);
-		Product prd2=new Product("servette",1000,646);
-		Product prrr=HTRepo.save(prd2);
-		List<Product> products= HTRepo.findAll();
-		System.out.println("products        dddddddddddddddddddddd"+products);
+
+		assertEquals("short",prd.getName());
+		assertEquals(1,HTRepo.findAll().size());
+
 
 
 	}
 
+
+	//integration test  for adding product process using MockMvc
+	@Test
+	public void addProductTest2() throws Exception {
+		ObjectMapper objMp=new ObjectMapper();
+		Product prd=Product.builder().name("casquette")
+				.quantity(1000)
+				.price(40).build();
+		MvcResult res=mvc.perform(post("/api/products/addProduct")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objMp.writeValueAsString(prd)))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.name",is("casquette")))
+				.andExpect(jsonPath("$.quantity",is(1000)))
+				.andExpect(jsonPath("$.price",is(40.0)))
+				.andReturn();
+
+		System.out.println(res.getResponse());
+
+
+	}
 }
--- src/test/java/com/integrationtest/first/integrationtest/h2TestRepository.java
+++ src/test/java/com/integrationtest/first/integrationtest/h2TestRepository.java
@@ -4,4 +4,5 @@ import com.integrationtest.first.integrationtest.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface h2TestRepository extends JpaRepository<Product,Integer> {
+   Product findByName(String name);
 }
